f95b06e394406a804fe45a824779eb5f448a8875
Tobias Walle <git@tobias.walle.email>
2021-12-17 12:22:54 +0100

    splitright & telescope hidden

--- lua/cfg/telescope.lua
+++ lua/cfg/telescope.lua
@@ -5,4 +5,10 @@ u.map('n', '<C-f>', '<cmd>lua require("telescope.builtin").live_grep()<cr>')
 u.map('n', '<C-c>', '<cmd>lua require("telescope.builtin").commands()<cr>')
 u.map('n', '<C-b>', '<cmd>lua require("telescope.builtin").buffers()<cr>')
 
-require('telescope').setup {}
+require('telescope').setup {
+  pickers = {
+    find_files = {
+      hidden = true
+    }
+  }
+}
