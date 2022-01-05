5a58c0f66ba04444695369df374be37146861651
Tobias Walle <git@tobias.walle.email>
2022-01-05 10:20:19 +0100

    fix linting & tests

--- lua/user/completion.lua
+++ lua/user/completion.lua
@@ -47,6 +47,7 @@ require('null-ls').setup {
   sources = {
     require('null-ls').builtins.formatting.prettierd,
     require('null-ls').builtins.formatting.eslint_d,
+    require('null-ls').builtins.diagnostics.eslint_d,
     require('null-ls').builtins.formatting.lua_format
   }
 }
