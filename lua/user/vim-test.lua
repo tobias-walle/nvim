d378ace813500f94970e762eea6834b5000c0eec
Tobias Walle <tobias.walle@maibornwolff.de>
2022-01-05 10:20:19 +0100

    fix linting & tests

--- lua/user/vim-test.lua
+++ lua/user/vim-test.lua
@@ -3,6 +3,7 @@ vim.g.ultest_use_pty = 1
 _G.configureJestTest = function()
 	vim.g['test#javascript#jest#executable'] = "yarn jest"
 	vim.g['test#javascript#runner'] = "jest"
+	vim.g['test#project_root'] = vim.fn.expand('%:p:h')
 end
 
 vim.cmd([[
