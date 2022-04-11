a9ca7732c7760859e25bd1641878496bf7aaf034
Tobias Walle <git@tobias.walle.email>
2022-04-11 08:09:11 +0200

    update treesitter

--- lua/user/other.lua
+++ lua/user/other.lua
@@ -38,7 +38,7 @@ vim.g.neoterm_default_mod = 'belowright'
 
 -- Treesitter
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = 'maintained',
+  ensure_installed = 'all',
   sync_install = false,
   highlight = {enable = true, disable = {}, additional_vim_regex_highlighting = false},
   indent = {enable = false},
