bdaaae4a958b8d1fd7821d77e56113b4ee313315
Tobias Walle <git@tobias.walle.email>
2022-09-17 13:36:04 +0200

    autocomplete file casing

--- lua/user/cmp-sources/filename.lua
+++ lua/user/cmp-sources/filename.lua
@@ -1,16 +1,19 @@
 local S = {}
 local cmp = require 'cmp'
+local casing = require('user.utils.casing')
 
-function S:new()
-  return setmetatable({}, {__index = S})
-end
+function S:new() return setmetatable({}, {__index = S}) end
 
-function S:get_keyword_pattern()
-  return [[\k\+]]
-end
+function S:get_keyword_pattern() return [[\k\+]] end
 
 function S:complete(params, callback)
-  callback({{kind = cmp.lsp.CompletionItemKind.File, label = vim.fn.expand('%:t:r')}})
+  local name = vim.fn.expand('%:t:r')
+  if (name == nil) then return end
+  local split = casing.splitLowerCase(name)
+  callback({
+    {kind = cmp.lsp.CompletionItemKind.File, label = casing.camelCase(split)},
+    {kind = cmp.lsp.CompletionItemKind.File, label = casing.pascalCase(split)}
+  })
 end
 
 return S
---
+++ lua/user/utils/casing.lua
@@ -0,0 +1,38 @@
+local M = {}
+
+--- @param names string | string[]
+--- @return string[]
+function M.splitLowerCase(names)
+  if (type(names) == 'string') then names = {names} end
+  local result = {}
+  for _, name in ipairs(names) do
+    for str in string.gmatch(name, '(%u?[^%u%._]+)') do table.insert(result, string.lower(str)) end
+  end
+  return result
+end
+
+--- @param names string[]
+--- @return string
+function M.pascalCase(names)
+  local result = ''
+  for _, word in ipairs(names) do
+    result = result .. string.sub(word, 1, 1):upper() .. string.sub(word, 2)
+  end
+  return result
+end
+
+--- @param names string[]
+--- @return string
+function M.camelCase(names)
+  local result = ''
+  for i, word in ipairs(names) do
+    if (i > 1) then
+      result = result .. string.sub(word, 1, 1):upper() .. string.sub(word, 2)
+    else
+      result = result .. word
+    end
+  end
+  return result
+end
+
+return M
