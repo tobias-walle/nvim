829df175d3fd0e00be367bc99d91d31ecb04edbb
Tobias Walle <git@tobias.walle.email>
2022-10-31 21:46:56 +0100

    add telescope file explorer

--- lua/user/explorer.lua
+++ lua/user/explorer.lua
@@ -119,7 +119,7 @@ require('neo-tree').setup({
     },
     follow_current_file = true,
     group_empty_dirs = false,
-    hijack_netrw_behavior = 'open_current',
+    hijack_netrw_behavior = 'disabled',
     use_libuv_file_watcher = true,
     window = {
       mappings = {
--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -1,17 +1,17 @@
 local M = {}
 
-require('telescope').load_extension('live_grep_args')
-require('telescope').load_extension('neoclip')
-
 local lga_actions = require('telescope-live-grep-args.actions')
+
 require('telescope').setup({
   defaults = {
-    file_ignore_patterns = { '.git/', 'yarn.lock', '.yarn' },
+    file_ignore_patterns = { '.git', 'yarn.lock', '.yarn' },
     layout_strategy = 'vertical',
     layout_config = { vertical = { width = 0.9 } },
   },
   pickers = {
-    find_files = { hidden = true },
+    find_files = {
+      hidden = true,
+    },
     live_grep = {
       vimgrep_arguments = {
         'rg',
@@ -35,15 +35,22 @@ require('telescope').setup({
         '--column',
         '--hidden',
       },
-      auto_quoting = true, -- enable/disable auto-quoting
-      -- override default mappings
-      -- default_mappings = {},
-      mappings = { -- extend mappings
+      auto_quoting = true,
+      mappings = {
         i = { ['<C-k>'] = lga_actions.quote_prompt() },
       },
     },
+    file_browser = {
+      hijack_netrw = false,
+      hidden = true,
+      respect_gitignore = false,
+    },
   },
 })
+
+require('telescope').load_extension('live_grep_args')
+require('telescope').load_extension('neoclip')
+require('telescope').load_extension('file_browser')
 
 function M.find_files_all()
   require('telescope.builtin').find_files({ no_ignore = true })
