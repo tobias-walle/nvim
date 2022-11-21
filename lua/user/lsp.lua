9af5253784189378f4fe706f02685009dbb5a090
Tobias Walle <tobias.walle@maibornwolff.de>
2022-11-21 22:10:31 +0100

    refactor lsp config

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -1,85 +1,79 @@
-local U = require('user.utils')
 local L = require('user.lsp.utils')
 
 local lspconfig = require('lspconfig')
+local null_ls = require('null-ls')
 
-require('mason').setup()
-require('mason-lspconfig').setup({
-  ensure_installed = {
-    'angularls',
-    'cssls',
-    'gopls',
-    'hls', -- Haskell
-    'jsonls',
-    'kotlin_language_server',
-    'omnisharp',
-    'pyright',
-    'r_language_server',
-    'rust_analyzer',
-    'sumneko_lua',
-    'tailwindcss',
-    'taplo',
-    'tsserver',
-    'yamlls',
-  },
-})
-require('mason-lspconfig').setup_handlers({
-  function(server_name)
-    lspconfig[server_name].setup({
-      on_attach = L.on_attach,
-    })
-  end,
-  ['angularls'] = require('user.lsp.typescript').setup_angular,
-  ['cssls'] = function(server_name)
-    lspconfig[server_name].setup({
-      on_attach = L.on_attach_disable_formatting,
-      capabilities = L.snippet_capabilities,
-    })
-  end,
-  ['hls'] = function(server_name)
-    lspconfig[server_name].setup({
-      on_attach = L.on_attach_disable_formatting,
-    })
-  end,
-  ['jsonls'] = function(server_name)
-    lspconfig[server_name].setup({
-      on_attach = L.on_attach_disable_formatting,
-      capabilities = L.snippet_capabilities,
-      settings = { json = { schemas = require('schemastore').json.schemas() } },
-    })
-  end,
-  ['omnisharp'] = function(server_name)
-    local pid = vim.fn.getpid()
-    local omnisharp_bin = '/usr/local/bin/omnisharp'
-    lspconfig[server_name].setup({
-      cmd = { omnisharp_bin, '--languageserver', '--hostPID', tostring(pid) },
-      on_attach = L.on_attach,
-    })
-  end,
-  ['rust_analyzer'] = function()
-    require('rust-tools').setup({
-      tools = { autoSetHints = false, hover_with_actions = false },
-
-      server = {
-        standalone = false,
+---@type LspConfig
+local config = {
+  lsp = {
+    ['yamlls'] = L.setup_default,
+    ['tailwindcss'] = L.setup_default,
+    ['pyright'] = L.setup_default,
+    ['r_language_server'] = L.setup_default,
+    ['gopls'] = L.setup_default,
+    ['kotlin_language_server'] = L.setup_default,
+    ['taplo'] = L.setup_default,
+    ['hls'] = L.setup_without_formatting,
+    ['tsserver'] = require('user.lsp.typescript').setup_typescript,
+    ['angularls'] = require('user.lsp.typescript').setup_angular,
+    ['cssls'] = function(name)
+      lspconfig[name].setup({
+        on_attach = L.on_attach_disable_formatting,
+        capabilities = L.snippet_capabilities,
+      })
+    end,
+    ['jsonls'] = function(name)
+      lspconfig[name].setup({
+        on_attach = L.on_attach_disable_formatting,
+        capabilities = L.snippet_capabilities,
+        settings = { json = { schemas = require('schemastore').json.schemas() } },
+      })
+    end,
+    ['omnisharp'] = function(name)
+      local pid = vim.fn.getpid()
+      local omnisharp_bin = '/usr/local/bin/omnisharp'
+      lspconfig[name].setup({
+        cmd = { omnisharp_bin, '--languageserver', '--hostPID', tostring(pid) },
         on_attach = L.on_attach,
-        settings = { ['rust-analyzer'] = { checkOnSave = { command = 'clippy' } } },
-      },
-    })
-  end,
-  ['sumneko_lua'] = function(server_name)
-    require('neodev').setup({})
-    lspconfig[server_name].setup({
-      on_attach = L.on_attach_disable_formatting,
-    })
-  end,
-  ['taplo'] = function(server_name)
-    lspconfig[server_name].setup({
-      on_attach = L.on_attach,
-    })
-  end,
-  ['tsserver'] = require('user.lsp.typescript').setup_typescript,
-})
+      })
+    end,
+    ['rust_analyzer'] = function()
+      require('rust-tools').setup({
+        tools = { autoSetHints = false, hover_with_actions = false },
+        server = {
+          standalone = false,
+          on_attach = L.on_attach,
+          settings = { ['rust-analyzer'] = { checkOnSave = { command = 'clippy' } } },
+        },
+      })
+    end,
+    ['sumneko_lua'] = function(name)
+      require('neodev').setup({})
+      lspconfig[name].setup({
+        on_attach = L.on_attach_disable_formatting,
+        settings = { Lua = { hint = { enable = true } } },
+      })
+    end,
+  },
+  null_ls = {
+    ['stylua'] = L.setup_null_ls_formatting,
+    ['black'] = L.setup_null_ls_formatting,
+    ['prettierd'] = L.setup_null_ls_formatting,
+    ['eslint_d'] = function(name)
+      return {
+        null_ls.builtins.formatting[name],
+        null_ls.builtins.code_actions[name],
+        null_ls.builtins.diagnostics[name].with({
+          condition = function(utils)
+            return utils.root_has_file({ '.eslintrc.js', '.eslintrc.cjs', '.eslintrc.yml', '.eslintrc.json' })
+          end,
+        }),
+      }
+    end,
+  },
+}
+
+L.apply_config(config)
 
 -- Nushell
 require('nu').setup({})
@@ -89,25 +83,6 @@ require('lsp_signature').setup({
   hint_enable = true,
   floating_window = false,
   hint_prefix = '',
-})
-
--- Null Ls
-require('null-ls').setup({
-  sources = {
-    require('null-ls').builtins.formatting.prettier,
-
-    require('null-ls').builtins.diagnostics.eslint_d.with({
-      condition = function(utils)
-        return utils.root_has_file({ '.eslintrc.js', '.eslintrc.cjs', '.eslintrc.yml', '.eslintrc.json' })
-      end,
-    }),
-    require('null-ls').builtins.formatting.eslint_d,
-    require('null-ls').builtins.code_actions.eslint_d,
-
-    require('null-ls').builtins.formatting.stylua,
-    require('null-ls').builtins.formatting.black,
-  },
-  on_attach = L.on_attach,
 })
 
 -- Better diagnostics
