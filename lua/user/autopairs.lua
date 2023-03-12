5afd5fc59255992195ddb004baf5d3f9bbc6bcb3
Tobias Walle <git@tobias.walle.email>
2023-03-12 12:47:02 +0100

    update autopairs settings

--- lua/user/autopairs.lua
+++ lua/user/autopairs.lua
@@ -1,8 +1,30 @@
 local npairs = require('nvim-autopairs')
-
-npairs.setup({ enable_moveright = false })
+npairs.setup({
+  enable_moveright = true,
+  fast_wrap = {},
+})
 
 require('nvim-ts-autotag').setup({
-  -- For WASM UI frameworks like Leptos in Rust
-  filetypes = { 'rust' },
+  filetypes = {
+    -- For WASM UI frameworks like Leptos in Rust
+    'rust',
+    -- Default values
+    'html',
+    'javascript',
+    'typescript',
+    'javascriptreact',
+    'typescriptreact',
+    'svelte',
+    'vue',
+    'tsx',
+    'jsx',
+    'rescript',
+    'xml',
+    'php',
+    'markdown',
+    'glimmer',
+    'handlebars',
+    'hbs',
+    'htmldjango',
+  },
 })
