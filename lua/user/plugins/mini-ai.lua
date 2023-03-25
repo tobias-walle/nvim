583f7a622efdd7e95792879777481d091b4649aa
Tobias Walle <tobias.walle@maibornwolff.de>
2023-03-25 17:20:44 +0100

    use mini.ai for text objects

--- lua/user/plugins/mini-ai.lua
+++ lua/user/plugins/mini-ai.lua
@@ -3,7 +3,18 @@ local plugin = {
   -- Custom surround text objects for example to replace text in ``
   'echasnovski/mini.ai',
   config = function()
-    require('mini.ai').setup()
+    local ai = require('mini.ai')
+    require('mini.ai').setup({
+      n_lines = 500,
+      custom_textobjects = {
+        o = ai.gen_spec.treesitter({
+          a = { '@block.outer', '@conditional.outer', '@loop.outer' },
+          i = { '@block.inner', '@conditional.inner', '@loop.inner' },
+        }, {}),
+        f = ai.gen_spec.treesitter({ a = '@function.outer', i = '@function.inner' }, {}),
+        c = ai.gen_spec.treesitter({ a = '@class.outer', i = '@class.inner' }, {}),
+      },
+    })
   end,
 }
 
