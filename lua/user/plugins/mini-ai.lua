01dc31fed3e7b95e11eb548223c48abd0277b531
Tobias Walle <tobias.walle@maibornwolff.de>
2023-05-04 19:50:45 +0200

    improve tag text-objects

--- lua/user/plugins/mini-ai.lua
+++ lua/user/plugins/mini-ai.lua
@@ -14,6 +14,7 @@ local plugin = {
         }, {}),
         f = ai.gen_spec.treesitter({ a = '@function.outer', i = '@function.inner' }, {}),
         c = ai.gen_spec.treesitter({ a = '@class.outer', i = '@class.inner' }, {}),
+        t = { '<([%p%w]-)%f[^<%w][^<>]->.-</%1>', '^<.->().*()</[^/]->$' },
       },
     })
   end,
