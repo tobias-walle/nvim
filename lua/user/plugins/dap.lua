c35cbc1b6221437b3cae750e51b75ef231386c58
Tobias Walle <tobias.walle@maibornwolff.de>
2023-05-04 20:45:57 +0200

    configure debugger for javascript

--- lua/user/plugins/dap.lua
+++ lua/user/plugins/dap.lua
@@ -1,7 +1,85 @@
 ---@type LazySpec
 local plugin = {
   'mfussenegger/nvim-dap',
-  event = 'VeryLazy',
+  dependencies = {
+    'rcarriga/nvim-dap-ui',
+    'theHamsta/nvim-dap-virtual-text',
+    {
+      'jay-babu/mason-nvim-dap.nvim',
+      dependencies = 'mason.nvim',
+      cmd = { 'DapInstall', 'DapUninstall' },
+    },
+    {
+      'folke/which-key.nvim',
+      opts = {
+        defaults = {
+          ['<leader>d'] = { name = '+debug' },
+        },
+      },
+    },
+  },
+  -- stylua: ignore
+  keys = {
+    { "<leader>du", function() require("dapui").toggle({ }) end, desc = "Dap UI" },
+    { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = {"n", "v"} },
+    { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
+    { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
+    { "<leader>dc", function() require("dap").continue() end, desc = "Continue" },
+    { "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
+    { "<leader>dg", function() require("dap").goto_() end, desc = "Go to line (no execute)" },
+    { "<leader>di", function() require("dap").step_into() end, desc = "Step Into" },
+    { "<leader>dj", function() require("dap").down() end, desc = "Down" },
+    { "<leader>dk", function() require("dap").up() end, desc = "Up" },
+    { "<leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
+    { "<leader>do", function() require("dap").step_out() end, desc = "Step Out" },
+    { "<leader>dO", function() require("dap").step_over() end, desc = "Step Over" },
+    { "<leader>dp", function() require("dap").pause() end, desc = "Pause" },
+    { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
+    { "<leader>ds", function() require("dap").session() end, desc = "Session" },
+    { "<leader>dt", function() require("dap").terminate() end, desc = "Terminate" },
+    { "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
+  },
+  config = function()
+    -- Mason integration
+    require('mason-nvim-dap').setup({
+      ensure_installed = { 'js-debug-adapter' },
+    })
+
+    -- Configure adapters
+    require('dap').adapters['pwa-node'] = {
+      type = 'server',
+      host = '127.0.0.1',
+      port = '${port}',
+      executable = {
+        command = 'js-debug-adapter',
+        args = { '${port}', '127.0.0.1' },
+      },
+    }
+
+    require('dap').configurations.javascript = {
+      {
+        type = 'pwa-node',
+        request = 'launch',
+        name = 'Launch file',
+        program = '${file}',
+        cwd = '${workspaceFolder}',
+      },
+    }
+
+    -- UI
+    local dap = require('dap')
+    local dapui = require('dapui')
+    dapui.setup()
+    dap.listeners.after.event_initialized['dapui_config'] = function()
+      dapui.open({})
+    end
+    dap.listeners.before.event_terminated['dapui_config'] = function()
+      dapui.close({})
+    end
+    dap.listeners.before.event_exited['dapui_config'] = function()
+      dapui.close({})
+    end
+  end,
 }
 
 return plugin
