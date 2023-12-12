a1d33aa31a5e271604dedbcd1d539e55d8d2efee
Tobias Walle <git@tobias.walle.email>
2023-12-12 20:51:30 +0100

    add keybindings for chatgpt

--- lua/user/plugins/chatgpt.lua
+++ lua/user/plugins/chatgpt.lua
@@ -3,7 +3,40 @@ local plugin = {
   {
     'robitx/gp.nvim',
     event = 'VeryLazy',
-    config = function() require('gp').setup() end,
+    config = function()
+      require('gp').setup({
+        hooks = {
+          UnitTests = function(gp, params)
+            local template = 'I have the following code from {{filename}}:\n\n'
+              .. '```{{filetype}}\n{{selection}}\n```\n\n'
+              .. 'Please respond by writing table driven unit tests for the code above.'
+            local agent = gp.get_command_agent()
+            gp.Prompt(
+              params,
+              gp.Target.append,
+              nil,
+              agent.model,
+              template,
+              agent.system_prompt
+            )
+          end,
+          Explain = function(gp, params)
+            local template = 'I have the following code from {{filename}}:\n\n'
+              .. '```{{filetype}}\n{{selection}}\n```\n\n'
+              .. 'Please respond by explaining the code above.'
+            local agent = gp.get_chat_agent()
+            gp.Prompt(
+              params,
+              gp.Target.popup,
+              nil,
+              agent.model,
+              template,
+              agent.system_prompt
+            )
+          end,
+        },
+      })
+    end,
   },
 }
 
