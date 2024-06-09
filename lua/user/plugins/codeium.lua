f8e21fd6889db4f09678efdb9eab38494d740135
Tobias Walle <tobias.walle@maibornwolff.de>
2024-06-09 18:28:44 +0200

    remove copilot and use codium if NVIM_ENABLE_AI=1 is set

--- lua/user/plugins/codeium.lua
+++ lua/user/plugins/codeium.lua
@@ -1,13 +1,13 @@
--- ---@type LazySpec
--- local plugin = {
---   'Exafunction/codeium.nvim',
---   dependencies = {
---     'nvim-lua/plenary.nvim',
---     'hrsh7th/nvim-cmp',
---   },
---   lazy = false,
---   config = function() require('codeium').setup({}) end,
--- }
---
--- return plugin
-return {}
+---@type LazySpec
+local plugin = {
+  'Exafunction/codeium.nvim',
+  enabled = os.getenv('NVIM_ENABLE_AI') == '1',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'hrsh7th/nvim-cmp',
+  },
+  lazy = false,
+  config = function() require('codeium').setup({}) end,
+}
+
+return plugin
--- lua/user/plugins/copilot.lua
+++
@@ -1,8 +0,0 @@
----@type LazySpec
-local plugin = {
-  'zbirenbaum/copilot.lua',
-  event = 'InsertEnter',
-  config = function() require('copilot').setup({}) end,
-}
-
-return plugin
