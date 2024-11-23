aaa493539821b9a93a10d645d117ea918e817858
Tobias Walle <git@tobias.walle.email>
2024-11-23 19:06:34 +0100

    nvim: use new ai.nvim config format

--- lua/user/plugins/ai.lua
+++ lua/user/plugins/ai.lua
@@ -5,7 +5,18 @@ local plugin = {
   enabled = true,
   event = 'BufEnter',
   config = function()
-    require('ai').setup({})
+    require('ai').setup({
+      default_model = 'azure:gpt-4o',
+      chat = {
+        model = 'azure:gpt-4o',
+      },
+      command = {
+        model = 'azure:gpt-4o',
+      },
+      completion = {
+        model = 'azure:gpt-4o-mini',
+      },
+    })
 
     -- Reload for development
     vim.keymap.set(
