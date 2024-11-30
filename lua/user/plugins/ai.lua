4296e585c0bc7fa21f72ca345d5bd97bbe592fc3
Tobias Walle <git@tobias.walle.email>
2024-11-30 21:16:02 +0100

    nvim: remove unnecessary model config

--- lua/user/plugins/ai.lua
+++ lua/user/plugins/ai.lua
@@ -7,12 +7,6 @@ local plugin = {
   config = function()
     require('ai').setup({
       default_model = 'azure:gpt-4o',
-      chat = {
-        model = 'azure:gpt-4o',
-      },
-      command = {
-        model = 'azure:gpt-4o',
-      },
       completion = {
         model = 'azure:gpt-4o-mini',
       },
