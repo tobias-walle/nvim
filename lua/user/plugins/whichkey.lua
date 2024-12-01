759a309ac6cd63c6c655eac48712189347a48be6
Tobias Walle <git@tobias.walle.email>
2024-12-01 12:30:13 +0100

    set whichkey delay

--- lua/user/plugins/whichkey.lua
+++ lua/user/plugins/whichkey.lua
@@ -2,7 +2,7 @@
 local plugin = {
   'folke/which-key.nvim',
   event = 'VeryLazy',
-  config = function() require('which-key').setup({}) end,
+  config = function() require('which-key').setup({ delay = 300 }) end,
 }
 
 return plugin
