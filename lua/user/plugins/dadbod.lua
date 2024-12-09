1845e3273e408cc850644e5c8748f98d1793c7dc
Tobias Walle <git@tobias.walle.email>
2024-12-09 17:49:07 +0100

    nvim: add dadbod

---
+++ lua/user/plugins/dadbod.lua
@@ -0,0 +1,35 @@
+---@type LazySpec
+local plugins = {
+  {
+    'kristijanhusak/vim-dadbod-ui',
+    dependencies = {
+      'tpope/vim-dadbod',
+      'kristijanhusak/vim-dadbod-completion',
+    },
+    cmd = {
+      'DBUI',
+      'DBUIToggle',
+      'DBUIAddConnection',
+      'DBUIFindBuffer',
+    },
+    init = function() vim.g.db_ui_use_nerd_fonts = 1 end,
+  },
+  {
+    'kristijanhusak/vim-dadbod-completion',
+    config = function()
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = { 'sql', 'mysql', 'plsql' },
+        callback = function()
+          require('cmp').setup.buffer({
+            sources = {
+              { name = 'vim-dadbod-completion' },
+              { name = 'buffer' },
+            },
+          })
+        end,
+      })
+    end,
+  },
+}
+
+return plugins
