20374fcb140945f18ce0fbecbde6e077751ceccb
Tobias Walle <tobias.walle@maibornwolff.de>
2024-12-09 17:45:56 +0100

    nvim: disable comment keymap for mini-bracketed

--- lua/user/plugins/mini-bracketed.lua
+++ lua/user/plugins/mini-bracketed.lua
@@ -2,7 +2,11 @@
 local plugin = {
   'echasnovski/mini.bracketed',
   lazy = false,
-  config = function() require('mini.bracketed').setup({}) end,
+  config = function()
+    require('mini.bracketed').setup({
+      comment = { suffix = '' },
+    })
+  end,
 }
 
 return plugin
