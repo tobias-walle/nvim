6ef3b47f9f756b207f09c8607de31fa09cc2c232
Tobias Walle <tobias.walle@maibornwolff.de>
2025-04-11 19:36:46 +0200

    nvim: update blink-cmp options

--- lua/user/plugins/blink-cmp.lua
+++ lua/user/plugins/blink-cmp.lua
@@ -5,23 +5,21 @@ local plugin = {
   lazy = false,
   dependencies = { 'rafamadriz/friendly-snippets' },
   opts = {
-    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-    -- 'super-tab' for mappings similar to vscode (tab to accept)
-    -- 'enter' for enter to accept
-    -- 'none' for no mappings
-    --
-    -- All presets have the following mappings:
-    -- C-space: Open menu or open docs if already open
-    -- C-n/C-p or Up/Down: Select next/previous item
-    -- C-e: Hide menu
-    -- C-k: Toggle signature help (if signature.enabled = true)
-    --
-    -- See :h blink-cmp-config-keymap for defining your own keymap
-    keymap = { preset = 'super-tab' },
+    keymap = {
+      preset = 'default', -- https://cmp.saghen.dev/configuration/keymap.html#default
+      ['<C-d>'] = { 'show', 'show_documentation', 'hide_documentation' },
+    },
 
     appearance = { nerd_font_variant = 'mono' },
 
-    completion = { documentation = { auto_show = false } },
+    completion = {
+      documentation = { auto_show = true },
+      accept = {
+        auto_brackets = {
+          enabled = false,
+        },
+      },
+    },
 
     snippets = { preset = 'luasnip' },
     sources = {
@@ -47,15 +45,21 @@ local plugin = {
             end,
           },
         },
+        lsp = {
+          fallbacks = {},
+        },
       },
     },
 
-    -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-    -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-    -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-    --
-    -- See the fuzzy documentation for more information
-    fuzzy = { implementation = 'prefer_rust_with_warning' },
+    fuzzy = {
+      implementation = 'prefer_rust_with_warning',
+      max_typos = function() return 1 end,
+      sorts = {
+        'exact', -- Always prioritize exact matches
+        'score',
+        'sort_text',
+      },
+    },
   },
   opts_extend = { 'sources.default' },
 }
