87b2caa727f8f50049e4bca3eb9821ecbdbe7587
Tobias Walle <tobias.walle@maibornwolff.de>
2025-04-19 23:57:03 +0200

    configure transparent background and fix app update

--- lua/user/plugins/theme.lua
+++ lua/user/plugins/theme.lua
@@ -6,6 +6,7 @@ local plugin = {
   priority = 1000,
   config = function()
     require('catppuccin').setup({
+      transparent_background = true,
       flavour = 'mocha',
       integrations = {
         aerial = true,
