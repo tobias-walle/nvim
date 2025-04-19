f830166ff27212f7e9dabff2e162b69c28cc0c21
Tobias Walle <tobias.walle@maibornwolff.de>
2025-04-20 00:16:26 +0200

    nvim: remove backdrop on char search

--- lua/user/plugins/flash.lua
+++ lua/user/plugins/flash.lua
@@ -4,7 +4,13 @@ local plugin = {
   'folke/flash.nvim',
   event = 'VeryLazy',
   ---@type Flash.Config
-  opts = {},
+  opts = {
+    modes = {
+      char = {
+        highlight = { backdrop = false },
+      },
+    },
+  },
   -- stylua: ignore
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
