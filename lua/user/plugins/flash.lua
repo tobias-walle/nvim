f457da8645cd27b8add9b27ff1d9bffb978256e5
Tobias Walle <tobias.walle@maibornwolff.de>
2025-04-19 08:11:16 +0200

    nvim: update flash keymaps

--- lua/user/plugins/flash.lua
+++ lua/user/plugins/flash.lua
@@ -4,24 +4,12 @@ local plugin = {
   'folke/flash.nvim',
   event = 'VeryLazy',
   ---@type Flash.Config
-  opts = {
-    search = {
-      -- when `true`, flash will be activated during regular search by default.
-      -- You can always toggle when searching with `require("flash").toggle()`
-      enabled = true,
-      highlight = { backdrop = true },
-      jump = { history = true, register = true, nohlsearch = true },
-      search = {
-        -- `forward` will be automatically set to the search direction
-        -- `mode` is always set to `search`
-        -- `incremental` is set to `true` when `incsearch` is enabled
-      },
-    },
-  },
+  opts = {},
   -- stylua: ignore
   keys = {
-    { "<tab>", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "ss", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "gs", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
   },
